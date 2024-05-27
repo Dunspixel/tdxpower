23e6ff568bb0765b0a166c688c24aaf41d707019
Dunspixel <53792348+Dunspixel@users.noreply.github.com>
2024-05-27 22:55:39 +0100

    Amended comment

--- tdxpower_calc.py
+++ tdxpower_calc.py
@@ -1,8 +1,10 @@
 # This is a recreation of the code used to calculate Power in Tetris DX, based on my analysis of the disassembly.
 # The original source code was probably a lot simpler and easy to understand than this.
 
-# At the moment, this can calculate the power rating for a single Marathon or Ultra game.
 # Single game power can be calculated from Score, Lines, and Soft-Drop points.
+# This is then added to (profile power * max(games played, 5)) and divided by max(games played, 5) + 1.
+
+# Currently, this only works for Marathon and Ultra modes.
 # There's some extra logic which is applied for 40Lines mode, but I haven't analysed that yet.
 
 global power
